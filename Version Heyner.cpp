862a7fe649611a0b8cc5d14cdb4c933360ec450e
Heynerx <92124218+Heynerx@users.noreply.github.com>
2021-10-08 19:06:06 -0500

    Version 1.2
    mejora de optimizacion

--- Version Heyner.cpp
+++ Version Heyner.cpp
@@ -12,12 +12,12 @@ void ceros();
 void segundoMenu();
 void comp();
 
-string tablero[3][5] = {};
-string posicion2[9] = {};
+string tablero[3][3] = {};
+string vectorTablero[9] = {};
 string nombre1, nombre2, nombre;
 bool continuar = true, boolDesicion = true;
 bool boolDosjugadores = true, o = true, boolCasillasmarc = true, boolMenu = true;
-int modoJuego = 0, posicion = 0, p = 0, v = 0, confirmarGanador = 0, tiempoDelay = 70000;
+int modoJuego = 0, posicion = 0, confirmarGanador = 0, tiempoDelay = 70000;
 int eleccion = 0;
 
 
@@ -26,7 +26,7 @@ int main()
     while (boolMenu)
     {
         ceros();
-        inicio();
+        primerMenu();
         mecanica();
     }
 }
@@ -80,42 +80,6 @@ void primerMenu()
 
 
 
-void segundoMenu(){
-system("clear");
-            
-            comp();
-            if (confirmarGanador == 1)
-            {
-            etc:
-                imprimirTablero();
-                cout << "\n\n¡Tres en raya!" << endl;
-                cout << "\nEl ganador es " << nombre << endl;
-                cout << "══════════════════════════════════" << endl;
-                cout << "Escriba el numero de la opcion: " << endl;
-                cout << "0.Volver al menu principal" << endl;
-                cout << "1.Para volver a jugar" << endl;
-                cout << "2.Salir" << endl;
-                cin >> eleccion;
-                if (eleccion == 1)
-                {
-                    ceros();
-                }
-                else if (eleccion == 0)
-                {
-                    system("clear");
-                    break;
-                }
-                else
-                {
-                    boolMenu = false;
-                    break;
-                }
-            }
-        }
-    }
-
-
-}
 
 
 //funcion encargada llenar el tablero con espacios para que no pierda la forma la impresion del tablero
@@ -148,15 +112,17 @@ system("clear");
 //llena todo con 0 para volver a jugar
     void ceros()
     {
+        int posVector = 0;
+
         confirmarGanador = 0;
-        v = 0;
+        
         for (int i = 0; i < 3; i++)
         {
 
             for (int j = 0; j < 3; j++)
             {
-                posicion2[v] = " ";
-                v++;
+                vectorTablero[posVector] = " ";
+                posVector++;
                 tablero[i][j] = " ";
             }
         }
@@ -178,6 +144,7 @@ void mecanica()
 
         while (boolDosjugadores == true)
         {
+            int p = 0;
 
             imprimirTablero();
             cout << "\n\nTurno de " << nombre1 << " X :" << endl;
@@ -186,7 +153,7 @@ void mecanica()
             {
                 cin >> posicion;
 
-                if (posicion2[posicion - 1] != " ")
+                if (vectorTablero[posicion - 1] != " ")
                 {
                     cout << "Esta casilla ya fue marcada, digite una vacia:" << endl;
                 }
@@ -196,7 +163,7 @@ void mecanica()
                 }
             }
             system("clear");
-            posicion2[posicion - 1] = "X";
+            vectorTablero[posicion - 1] = "X";
 
             p = 0;
             for (int i = 0; i < 3; i++)
@@ -204,7 +171,7 @@ void mecanica()
 
                 for (int j = 0; j < 3; j++)
                 {
-                    tablero[i][j] = posicion2[p];
+                    tablero[i][j] = vectorTablero[p];
                     p++;
                 }
             }
@@ -221,7 +188,7 @@ void mecanica()
             {
                 cin >> posicion;
 
-                if (posicion2[posicion - 1] != " ")
+                if (vectorTablero[posicion - 1] != " ")
                 {
                     cout << "Esta casilla ya fue marcada, digite una vacia:" << endl;
                 }
@@ -230,29 +197,64 @@ void mecanica()
                     break;
                 }
             }
-            posicion2[posicion - 1] = "O";
+            vectorTablero[posicion - 1] = "O";
             p = 0;
             for (int i = 0; i < 3; i++)
             {
 
                 for (int j = 0; j < 3; j++)
                 {
-                    tablero[i][j] = posicion2[p];
+                    tablero[i][j] = vectorTablero[p];
                     p++;
                 }
             }
-            segundoMenu();
+            system("clear");
+            
+            comp();
+            if (confirmarGanador == 1)
+            {
+            etc:
+                imprimirTablero();
+                segundoMenu();
+                cin >> eleccion;
+                if (eleccion == 1)
+                {
+                    ceros();
+                }
+                else if (eleccion == 0)
+                {
+                    system("clear");
+                    break;
+                }
+                else
+                {
+                    boolMenu = false;
+                    break;
+                }
+            }
+        }
+    }
+
+            
+}
+
+void segundoMenu(){
+cout << "\n\n¡Tres en raya!" << endl;
+                cout << "\nEl ganador es " << nombre << endl;
+                cout << "══════════════════════════════════" << endl;
+                cout << "Escriba el numero de la opcion: " << endl;
+                cout << "0.Volver al menu principal" << endl;
+                cout << "1.Para volver a jugar" << endl;
+                cout << "2.Salir" << endl;
+
 }
 
 
-//
 void comp()
 {
+    int j = 0;
     for (int i = 0; i < 3; i++)
     {
-
-        for (int j = 0; j < 3; j++)
-        {
 
             if ((tablero[i][j] == "X" && tablero[i][j + 1] == "X" && tablero[i][j + 2] == "X") || (tablero[i][j] == "O" && tablero[i][j + 1] == "O" && tablero[i][j + 2] == "O"))
             {
@@ -264,16 +266,17 @@ void comp()
                 confirmarGanador = 1;
             }
 
-            if ((posicion2[0] == "X" && posicion2[4] == "X" && posicion2[8] == "X") || (posicion2[0] == "O" && posicion2[4] == "O" && posicion2[8] == "O"))
+            if ((vectorTablero[0] == "X" && vectorTablero[4] == "X" && vectorTablero[8] == "X") || (vectorTablero[0] == "O" && vectorTablero[4] == "O" && vectorTablero[8] == "O"))
             {
                 confirmarGanador = 1;
             }
 
-            if ((posicion2[6] == "X" && posicion2[4] == "X" && posicion2[2] == "X") || (posicion2[6] == "O" && posicion2[4] == "O" && posicion2[2] == "O"))
+            if ((vectorTablero[6] == "X" && vectorTablero[4] == "X" && vectorTablero[2] == "X") || (vectorTablero[6] == "O" && vectorTablero[4] == "O" && vectorTablero[2] == "O"))
             {
                 confirmarGanador = 1;
             }
-        }
+        
     }
 }
+
 
