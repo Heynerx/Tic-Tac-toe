cf57e76060a39ed6226ac423fb8205757a33d913
IVDAL2NIGHT <90803113+IVDAL2NIGHT@users.noreply.github.com>
2021-10-11 19:21:52 -0500

    Update Version Daniel.cpp
    ahí está el menu provicional y la opción de escoger para dos jugadores o contr ala "máquina", falta arreglar la función "ganar" que da errores

--- Version Daniel.cpp
+++ Version Daniel.cpp
@@ -3,89 +3,143 @@
 #include <stdlib.h>
 
 void loop(char c[3][3]); 
+void loop2(char c[3][3]); 
 void intro_primera(char c[3][3]); 
 void tablero(char c[3][3]); 
 void introducir(char c[3][3]); 
+void introducir2(char c[3][3]);
 void introducir_IA(char c[3][3]); 
-int  ganar(char c[3][3]); 
+int  ganar(char c[3][3]);  
 
 int main(){
-	char c[3][3];
-	loop(c);
-	return 0;
+char c[3][3]; 
+int a; 
+
+printf("\nBienvenido a triqui, desea jugar contra la maquina o contra un segundo jugador ?");
+printf("\nSi desea jugar contra la maquina pulse: 0");
+printf("\nSi desea jugar contra un segundo jugador pulse: 1");
+printf("\n\nSeleccione una respuesta: ");
+scanf("%d",&a);  
+
+if(a==0){
+	loop(c); 
+}
+else{
+	loop2(c); 
+}
+
+return 0; 
 }
 
 void loop (char c[3][3]){
 	
-	int i, j; 
-	i = 0; 
+	int i,j; 
+	i=0; 
 	
 	intro_primera(c);
-	do {
+	do{
 		system("cls"); 
 		tablero(c); 
 		
-		if (i % 2 == 0) {
+		if(i%2==0){
 			introducir(c); 
 		}
-		else {
+		else{
 			introducir_IA(c); 
 		}
 		
-		j = ganar(c); 
+		j=ganar(c); 
 		
 		i++; 
 		
-	} while (1 <= 9 && j == 2); 
+	}while(1<=9 && j==2); 
 	
-	system("cls");
-	tablero(c);
+	system("cls"); 
+	tablero(c); 
 	
-	if (j == 0) {
+	if(j=0){
 		printf("\n\nFelicidades has ganado la partida!\n ");
 	}
-	else if (j == 1) {
+	else if(j=1){
 		printf("\n\nintentalo otra vez, has perdido la partida!\n");
 	}
-	else{
+	else{ 
 		printf("\n\nHa sido un empate!\n"); 
 	}
 }
 
-void intro_primera(char c[3][3]){
-	int i = 0;
-	int j = 0; 
-	char aux = 0; 
-	aux = '1'; 
+void loop2 (char c[3][3]){
 	
-	for (i = 0; i < 3; i++) {
-		for (j = 0; j < 3; j++) {
-			c[i][j] = aux++; 	
+	int i,j; 
+	i=0; 
+	
+	intro_primera(c);
+	do{
+		system("cls"); 
+		tablero(c); 
+		
+		if(i%2==0){
+			introducir(c); 
+		}
+		else{
+			introducir2(c); 
+		}
+		
+		j=ganar(c); 
+		
+		i++; 
+		
+	}while(1<=9 && j==2); 
+	
+	system("cls"); 
+	tablero(c); 
+	
+	if(j=0){
+		printf("\n\nFelicidades has ganado la partida!\n ");
+	}
+	else if(j=1){
+		printf("\n\nintentalo otra vez, has perdido la partida!\n");
+	}
+	else{ 
+		printf("\n\nHa sido un empate!\n"); 
+	}
+	
+}
+
+void intro_primera(char c[3][3]){
+	int i=0;
+	int j=0; 
+	char aux=0; 
+	aux='1'; 
+	
+	for(i=0; i<3; i++){
+		for(j=0; j<3; j++){
+			c[i][j]=aux++; 	
 		}
 	}
 }
 
 void introducir(char c[3][3]){
 	
-	int i = 0;
-	int j = 0; 
-	int k = 0; 
+	int i=0;
+	int j=0; 
+	int k=0; 
 	char aux; 
 	
-	do {
-		do {
-			printf("\n\nColocar un ficha: "); 
+	do{
+		do{
+			printf("\n\ncolocar un ficha: "); 
 			fflush(stdin);
 			scanf("%c", &aux); 
-		} while (aux < '1' || aux > '9');  
+		}while (aux < '1' || aux > '9');  
 		
-		k = 0;
+		k=0;
 		
 		switch(aux){
 				case '1':{
-				i = 0;
-				j = 0;
-				if (c[i][j] == 'X' || c[i][j] == 'O'){
+				i=0;
+				j=0;
+				if (c[i][j]== 'X' || c[i][j]== 'O'){
 					k=1;
 					printf("la casilla seleccionada ya esta ocupada, por favor inentelo de nuevo\n\n"); 	
 				}
@@ -93,9 +147,9 @@ void introducir(char c[3][3]){
 			}
 			
 				case '2':{
-				i = 0;
-				j = 1;
-				if (c[i][j] == 'X' || c[i][j] == 'O'){
+				i=0;
+				j=1;
+				if (c[i][j]== 'X' || c[i][j]== 'O'){
 					k=1;
 					printf("la casilla seleccionada ya esta ocupada, por favor inentelo de nuevo\n\n"); 	
 				}
@@ -172,8 +226,120 @@ void introducir(char c[3][3]){
 				break;
 			}
 		}
-	}while(k == 1); 
+	}while(k==1); 
 	c[i][j] = 'X'; 
+	
+}
+
+void introducir2(char c[3][3]){
+	
+	int i=0;
+	int j=0; 
+	int k=0; 
+	char aux; 
+	
+	do{
+		do{
+			printf("\n\ncolocar un ficha: "); 
+			fflush(stdin);
+			scanf("%c", &aux); 
+		}while (aux < '1' || aux > '9');  
+		
+		k=0;
+		
+		switch(aux){
+				case '1':{
+				i=0;
+				j=0;
+				if (c[i][j]== 'X' || c[i][j]== 'O'){
+					k=2;
+					printf("la casilla seleccionada ya esta ocupada, por favor inentelo de nuevo\n\n"); 	
+				}
+				break;
+			}
+			
+				case '2':{
+				i=0;
+				j=1;
+				if (c[i][j]== 'X' || c[i][j]== 'O'){
+					k=2;
+					printf("la casilla seleccionada ya esta ocupada, por favor inentelo de nuevo\n\n"); 	
+				}
+				break;
+			}
+			
+				case '3':{
+				i=0;
+				j=2;
+				if (c[i][j]== 'X' || c[i][j]== 'O'){
+					k=2;
+					printf("la casilla seleccionada ya esta ocupada, por favor inentelo de nuevo\n\n"); 	
+				}
+				break;
+			}
+			
+				case '4':{
+				i=1;
+				j=0;
+				if (c[i][j]== 'X' || c[i][j]== 'O'){
+					k=2;
+					printf("la casilla seleccionada ya esta ocupada, por favor inentelo de nuevo\n\n"); 	
+				}
+				break;
+			}
+			
+				case '5':{
+				i=1;
+				j=1;
+				if (c[i][j]== 'X' || c[i][j]== 'O'){
+					k=2;
+					printf("la casilla seleccionada ya esta ocupada, por favor inentelo de nuevo\n\n"); 	
+				}
+				break;
+			}
+			
+				case '6':{
+				i=1;
+				j=2;
+				if (c[i][j]== 'X' || c[i][j]== 'O'){
+					k=2;
+					printf("la casilla seleccionada ya esta ocupada, por favor inentelo de nuevo\n\n"); 	
+				}
+				break;
+			}
+			
+				case '7':{
+				i=2;
+				j=0;
+				if (c[i][j]== 'X' || c[i][j]== 'O'){
+					k=2;
+					printf("la casilla seleccionada ya esta ocupada, por favor inentelo de nuevo\n\n"); 	
+				}
+				break;
+			}
+			
+				case '8':{
+				i=2;
+				j=1;
+				if (c[i][j]== 'X' || c[i][j]== 'O'){
+					k=2;
+					printf("la casilla seleccionada ya esta ocupada, por favor inentelo de nuevo\n\n"); 	
+				}
+				break;
+			}
+			
+				case '9':{
+				i=2;
+				j=2;
+				if (c[i][j]== 'X' || c[i][j]== 'O'){
+					k=2;
+					printf("la casilla seleccionada ya esta ocupada, por favor inentelo de nuevo\n\n"); 	
+				}
+				break;
+			}
+		}
+	}while(k==2); 
+	c[i][j] = 'O'; 
 	
 }
 
@@ -183,17 +349,17 @@ void introducir_IA(char c[3][3]){
 	int i,j,k; 
 	srand(NULL); 
 	
-	do {
+	do{
 		i = rand()%3;
 		j = rand()%3;
 		k = 0; 
 		
-			if (c[i][j] == 'X' || c[i][j] == 'O'){
-					k = 1;
+			if (c[i][j]== 'X' || c[i][j]== 'O'){
+					k=1;
 			}	
-	} while (k==1); 
+	}while(k==1); 
 	
-	c[i][j] = 'O'; 
+	c[i][j]='O'; 
 }
 
 void tablero(char c[3][3]){
@@ -302,7 +468,4 @@ int ganar(char c[3][3]){
 	}
 	return 2; 
 }
-
-
-
 
